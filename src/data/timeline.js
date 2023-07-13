a0c5cfaaeb1417d58c72115c191b0d97a5ea6571
AlexG2G <alexgreenfield22@gmail.com>
2023-07-13 16:56:32 +1000

    Update

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,13 +1,13 @@
 export default [
 	{
-		year: '2022-2024',
+		year: '2022-2025',
 		title: "Bachelor's Degree in Software Engineering",
 		duration: '3 years',
 		details:
 			'I am pursuing the knowledge that a software engineering degree has to offer. I am learning about software engineering, software design, database design, an array of programming, cybersecurity and system administration.',
 	},
 	{
-		year: '2018',
+		year: '2016',
 		title: 'First Line of Code',
 		duration: 'the beginning',
 		details:
