994a5a456ecf338a8b34bd2abef5a7ebcea0a161
AlexG2G <alexgreenfield22@gmail.com>
2023-07-13 16:55:43 +1000

    Update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,8 +6,8 @@ function Intro() {
          <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Alex</h1>
          <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
-            Alex is a Software Engineer who is currently focusing on Web Development, Python Programming,
-            System Analysis & Modelling, Software Design and Cybersecurity.
+            Alex is a Software Engineer who is currently focusing on Web Design, Programming,
+            Network Architecture and Network and System Admin.
             He has many sought after skills in the ICT sector such as teamwork, collaboration,
             problem-solving, and communication skills. He is currently studying a Bachelor of
             Software Engineering at the University of Canberra.
